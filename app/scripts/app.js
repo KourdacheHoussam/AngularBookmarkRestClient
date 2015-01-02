4f07c5958b6cbf011cd4be41c4d73eddbf599959
HoussamKourdache <kourdache88@yahoo.fr>
2015-01-02 18:01:00 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularBookmarkRestClientApp
+ * @description
+ * # angularBookmarkRestClientApp
+ *
+ * Main module of the application.
+ */
+var app=angular.module('angularBookmarkRestClientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ]);
+
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
